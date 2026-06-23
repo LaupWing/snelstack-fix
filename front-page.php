50d5c2b672cd84aca776c59d6563d7b68a253a1e
laupwing <laupwing@gmail.com>
2026-06-23 08:26:11 +0200

    Cleanup: gitignore build/, reset to clean base

--- front-page.php
+++ front-page.php
@@ -1,7 +1,5 @@
 <?php get_header(); ?>
 
-<div class="pt-32 px-8 text-white">
-    <h1 class="text-4xl font-bold">Hello World</h1>
-</div>
+<?php while (have_posts()) : the_post(); the_content(); endwhile; ?>
 
 <?php get_footer(); ?>
