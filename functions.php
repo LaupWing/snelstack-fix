649cfe6d75358c43fd12eaa6f3b360a88dfd696f
laupwing <laupwing@gmail.com>
2026-06-23 14:18:20 +0200

    Add editor styles: Inter font, slot/hero overrides

--- functions.php
+++ functions.php
@@ -10,6 +10,9 @@ add_action('after_setup_theme', function () {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     register_nav_menus(['primary' => __('Primary Menu', 'snel')]);
+    add_theme_support('editor-styles');
+    add_editor_style('build/index.css');
+    add_editor_style('src/editor.css');
 });
 
 add_action('init', function () {
