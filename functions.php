77ad7905c6ab830219eaae6a660279533422c9a3
laupwing <laupwing@gmail.com>
2026-06-23 07:35:09 +0200

    Fonts: move Google Fonts to wp_enqueue_style to avoid CSS waterfall

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('snelstack', get_template_directory_uri() . '/build/index.css', [], '1.0.0');
+    wp_enqueue_style('snelstack-fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap', [], null);
+    wp_enqueue_style('snelstack', get_template_directory_uri() . '/build/index.css', ['snelstack-fonts'], '1.0.0');
 });
